33c37180d4cca1ae10e4eb4cac37dce0e9c56564
KonH <konh@yandex.ru>
2020-08-08 20:23:30 +0700

    Remove current implementations

--- Assets/Scripts/Prototype2/Logic/ControlsState.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-namespace TetrisGame.Logic {
-	public struct ControlsState {
-		public Vector2Int MoveInput;
-		public bool       Rotate;
-	}
-}
--- Assets/Scripts/Prototype2/Logic/SessionState.cs
+++
@@ -1,7 +0,0 @@
-namespace TetrisGame.Logic {
-	public struct SessionState {
-		public ControlsState Controls;
-		public FigureState   Figure;
-		public bool[,]       Field;
-	}
-}

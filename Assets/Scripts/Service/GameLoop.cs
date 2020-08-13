df983d1abe72e235c25535f365eafdcca4c7c328
KonH <konh@yandex.ru>
2020-08-13 23:53:53 +0700

    Perform deconstruct on collisions & bottom, also revert move or rotate

--- Assets/Scripts/Service/GameLoop.cs
+++ Assets/Scripts/Service/GameLoop.cs
@@ -3,21 +3,27 @@ using UnityEngine;
 
 namespace TetrisGame.Service {
 	public sealed class GameLoop {
-		readonly FigureSpawner _spawner;
-		readonly FigureMover   _mover;
-		readonly FigureRotator _rotator;
-		readonly GameState     _state;
+		readonly FigureSpawner       _spawner;
+		readonly FigureMover         _mover;
+		readonly FigureRotator       _rotator;
+		readonly CollisionDetector   _collisionDetector;
+		readonly LimitDetector       _limitDetector;
+		readonly FigureDeconstructor _deconstructor;
+		readonly GameState           _state;
 
 		public GameLoop(int width, int height, Vector2[][] figures, GameState state) {
-			_spawner = new FigureSpawner(width, height, figures);
-			_mover   = new FigureMover();
-			_rotator = new FigureRotator();
-			_state   = state;
+			_spawner           = new FigureSpawner(width, height, figures);
+			_mover             = new FigureMover();
+			_rotator           = new FigureRotator();
+			_collisionDetector = new CollisionDetector();
+			_limitDetector     = new LimitDetector(width);
+			_deconstructor     = new FigureDeconstructor();
+			_state             = state;
 		}
 
 		public void Update(float dt) {
-			MoveByInput();
 			TrySpawnNewFigure();
+			MoveByInput();
 			ResetInput();
 		}
 
@@ -30,22 +36,51 @@ namespace TetrisGame.Service {
 		void MoveByInput() {
 			switch ( _state.Input ) {
 				case InputState.MoveLeft:
-					_mover.Move(_state.Figure, Vector2.left);
+					MoveHorizontal(false);
 					break;
 
 				case InputState.MoveRight:
-					_mover.Move(_state.Figure, Vector2.right);
+					MoveHorizontal(true);
 					break;
 
 				case InputState.SpeedUp:
-					_mover.Move(_state.Figure, Vector2.down);
+					MoveVertical();
 					break;
 
 				case InputState.Rotate:
 					_rotator.Rotate(_state.Figure);
+					if ( ShouldDeconstruct() ) {
+						_rotator.RotateBack(_state.Figure);
+					}
 					break;
 			}
 		}
+
+		void MoveHorizontal(bool right) {
+			var step = right ? Vector2.right : Vector2.left;
+			_mover.Move(_state.Figure, step);
+			if ( ShouldDeconstruct() ) {
+				_mover.Move(_state.Figure, -step);
+			}
+		}
+
+		void MoveVertical(float distance = 1.0f) {
+			var step = Vector2.down * distance;
+			_mover.Move(_state.Figure, step);
+			if ( ShouldDeconstruct() ) {
+				_mover.Move(_state.Figure, -step);
+				Deconstruct();
+			}
+		}
+
+		void Deconstruct() {
+			_deconstructor.Place(_state.Field, _state.Figure);
+			_state.Figure.Elements.Clear();
+		}
+
+		bool ShouldDeconstruct() =>
+			_collisionDetector.HasCollisions(_state.Field, _state.Figure) ||
+			_limitDetector.IsLimitReached(_state.Figure);
 
 		void ResetInput() {
 			_state.Input = InputState.None;

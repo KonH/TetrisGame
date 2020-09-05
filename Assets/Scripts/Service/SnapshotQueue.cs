5d4f08fccd02bd360b7ace0a1d0698289c4736a9
KonH <konh@yandex.ru>
2020-09-06 00:56:36 +0700

    Remove old implementation

--- Assets/Scripts/Service/SnapshotQueue.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Collections.Generic;
-
-namespace TetrisGame.Service {
-	[Serializable]
-	public sealed class SnapshotQueue {
-		public List<SnapshotEntry> Queue = new List<SnapshotEntry>();
-	}
-}
